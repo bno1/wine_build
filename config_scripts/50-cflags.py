a21def30e244c95d18972a80d39a50096b7c33e6
bno1 <ealex95@gmail.com>
2020-03-16 20:47:05 +0200

    Minor updates

--- config_scripts/50-cflags.py
+++ config_scripts/50-cflags.py
@@ -1,17 +1,21 @@
+CFLAGS = ' -O3 -pipe'
+LDFLAGS = ' -pipe'
+
+
 def configure_options(project_name, options):
     if project_name.startswith('dxvk-'):
         for arg in ['c_args', 'cpp_args']:
             flags = options.get(arg, '')
-            options[arg] = flags + ' -O3 -pipe'
+            options[arg] = flags + CFLAGS
 
         for arg in ['c_link_args', 'cpp_link_args']:
             flags = options.get(arg, '')
-            options[arg] = flags + ' -pipe'
+            options[arg] = flags + LDFLAGS
     elif project_name.startswith('wine-'):
         for arg in ['CFLAGS', 'CPPFLAGS']:
             flags = options.get(arg, '')
-            options[arg] = flags + ' -O3 -pipe'
+            options[arg] = flags + CFLAGS
 
         for arg in ['LDFLAGS']:
             flags = options.get(arg, '')
-            options[arg] = flags + ' -pipe'
+            options[arg] = flags + LDFLAGS
